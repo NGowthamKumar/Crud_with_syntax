60e2e8c2ae78f9541b5ddec504bf22adf5e946b4
N Gowtham Kumar <48080545+NGowthamKumar@users.noreply.github.com>
2020-11-03 18:43:50 +0530

    Update UserSchema.js

--- src/model/UserSchema.js
+++ src/model/UserSchema.js
@@ -6,5 +6,4 @@ const UserSchema = new mongoose.Schema({
     password : { type: String, required: true }},
     { versionKey: false });
   
-  
-  module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
