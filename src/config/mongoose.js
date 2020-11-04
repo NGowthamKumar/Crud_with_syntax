758df740cdc20487fa5831964b33c35d72fe2f55
N Gowtham Kumar <48080545+NGowthamKumar@users.noreply.github.com>
2020-11-04 10:08:44 +0530

    Update mongoose.js

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/mydb',{'useUnifiedTopology' : true ,'createNewIndex' : true , 'useNewUrlParser' : true },function(err){
+mongoose.connect('mongodb://localhost:27017/mydb',{'useUnifiedTopology' : true , 'useNewUrlParser' : true },function(err){
     if(err) console.log("Can't connect MongoDB");
    else console.log("Connected to MongoDB...");
 }); 
